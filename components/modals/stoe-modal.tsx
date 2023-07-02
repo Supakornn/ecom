acc2c0462595a0d409352237849b147cf0c20079
Supakorn <supakorn0728@gmail.com>
2023-07-02 14:28:15 +0700

    form

--- components/modals/stoe-modal.tsx
+++ components/modals/stoe-modal.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import * as z from "zod";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  name: z.string().min(1)
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { name: "" }
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {};
 
   return (
     <Modal
