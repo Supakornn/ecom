9ad415f82b8a63e16429cda2da5fc5dd941892cf
supakorn <supakorn0728@gmail.com>
2023-07-05 22:59:32 +0700

    updated

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -14,7 +14,7 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading title="Billboards (0)" description="Manage billboards for your store" />
-        <Button onClick={() => {}}>
+        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 2-4" />
           Add New
         </Button>
