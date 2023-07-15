d98d078c281f047082d17cc246e6ae3ce5580a0f
Supakorn <supakorn0728@gmail.com>
2023-07-15 19:50:07 +0700

    orders

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/column.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type OrderColumn = {
+  id: string;
+  address: string;
+  phone: string;
+  products: string;
+  totalPrice: string;
+  isPaid: boolean;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Products"
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone"
+  },
+  {
+    accessorKey: "address",
+    header: "Address"
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total Price"
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid"
+  }
+];
